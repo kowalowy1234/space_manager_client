c8aa8e2d26c036553c8c2ddfee22361fd3114146
kowalowy1234 <kowalowy1234@gmail.com>
2026-06-07 12:41:03 +0200

    created main page components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './pages/landing-page/landing-page';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { SignInPage } from './pages/sign-in-page/sign-in-page';
+import { SignUpPage } from './pages/sign-up-page/sign-up-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LandingPage
+    },
+    {
+        path: "sign-in",
+        component: SignInPage
+    },
+    {
+        path: "sign-up",
+        component: SignUpPage
+    },
+    {
+        path: "dashboard",
+        component: Dashboard
+    }
+];
