64ca7557191fe11e822fe95fa669cdaca3bc65e6
kowalowy1234 <kowalowy1234@gmail.com>
2026-06-09 18:34:21 +0200

    navigation for landing page

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { SkeletonModule } from 'primeng/skeleton';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-landing-page',
   imports: [
     ButtonModule,
     SkeletonModule,
-    AnimateOnScrollModule
-  ],
+    AnimateOnScrollModule,
+    RouterLink
+],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
