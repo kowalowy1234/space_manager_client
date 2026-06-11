ba58a7f8bfe5cb70f3fc5d48c88e285c59f91803
kowalowy1234 <kowalowy1234@gmail.com>
2026-06-11 19:14:31 +0200

    configure envs

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://loalhost:3000/api'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://loalhost:3000/api'
+};
